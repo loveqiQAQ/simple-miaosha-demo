7e1276788dbda014ba0b2a0057142040bb55fd95
李方明 <firminli247@gmail.com>
2018-12-16 02:24:16 +0800

    init repository and add spring boot starter

---
+++ src/main/java/me/loveqi/miaoshaproject/App.java
@@ -0,0 +1,18 @@
+package me.loveqi.miaoshaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Hello world!
+ *
+ * @author loveqi
+ */
+@EnableAutoConfiguration
+public class App {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
