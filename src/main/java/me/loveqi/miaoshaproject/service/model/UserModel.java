7c7fbe95a1b0be7b0ce5e1f889ab61ca5200dc2d
李方明 <firminli247@gmail.com>
2018-12-18 22:42:36 +0800

    add userModel class，this class Do not interact with the front desk，Only be used for the business layer

---
+++ src/main/java/me/loveqi/miaoshaproject/service/model/UserModel.java
@@ -0,0 +1,90 @@
+package me.loveqi.miaoshaproject.service.model;
+
+/**
+ * Description: UserModel 类，该类才是真正意义上处理业务逻辑的一个核心模型
+ *
+ * @author loveqi
+ * @date 2018-12-18 00:03
+ */
+public class UserModel {
+
+    private Integer id;
+
+    private String name;
+
+    private Byte gender;
+
+    private Integer age;
+
+    private String telphone;
+
+    private String registerMode;
+
+    private String thirdPartyId;
+
+    private String encrptPassword;
+
+    public String getEncrptPassword() {
+        return encrptPassword;
+    }
+
+    public void setEncrptPassword(String encrptPassword) {
+        this.encrptPassword = encrptPassword;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte getGender() {
+        return gender;
+    }
+
+    public void setGender(Byte gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getRegisterMode() {
+        return registerMode;
+    }
+
+    public void setRegisterMode(String registerMode) {
+        this.registerMode = registerMode;
+    }
+
+    public String getThirdPartyId() {
+        return thirdPartyId;
+    }
+
+    public void setThirdPartyId(String thirdPartyId) {
+        this.thirdPartyId = thirdPartyId;
+    }
+}
